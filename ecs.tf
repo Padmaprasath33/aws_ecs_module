cc12e76cff0cce530cb39114860e5e531c72cedc
Padmaprasath <padmaprasath33@gmail.com>
2023-12-05 18:34:02 +0530

    Added ECS related configs

---
+++ ecs.tf
@@ -0,0 +1,3 @@
+resource "aws_ecs_cluster" "cohort_demo_cluster" {
+  name = var.cohort_demo_cluster_name
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "cohort_demo_ecr_crr_region" {
   description = "ECR cross region replication region"
   //default     = ""
 }
+
+variable "cohort_demo_cluster_name" {
+  description = "ECS cluster name"
+  //default     = ""
+}
