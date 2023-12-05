44ba69353909eadd6cbcf3b8bdbdded6c2b302d1
Padmaprasath <padmaprasath33@gmail.com>
2023-12-05 18:43:52 +0530

    Added ECS related configs

--- ecs.tf
+++ ecs.tf
@@ -1,3 +1,31 @@
-resource "aws_ecs_cluster" "cohort_demo_cluster" {
-  name = var.cohort_demo_cluster_name
+resource "aws_ecs_cluster" "cohort_demo_ecs_cluster" {
+  name = var.cohort_demo_ecs_cluster_name
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
+
+  configuration {
+    execute_command_configuration {
+      kms_key_id = aws_kms_key.cohort_demo_kms.arn
+      logging    = "DEFAULT"
+
+      log_configuration {
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group_name     = aws_cloudwatch_log_group.cohort_demo_ecs_log_group.name
+      }
+    }
+  }
 }
+
+resource "aws_kms_key" "cohort_demo_kms" {
+  description             = "KMS key for cohort demo"
+  deletion_window_in_days = 10
+}
+
+resource "aws_cloudwatch_log_group" "cohort_demo_ecs_log_group" {
+  name = var.cohort_demo_ecs_log_group
+}
+
+
--- variables.tf
+++ variables.tf
@@ -3,7 +3,12 @@ variable "cohort_demo_ecr_crr_region" {
   //default     = ""
 }
 
-variable "cohort_demo_cluster_name" {
+variable "cohort_demo_ecs_cluster_name" {
   description = "ECS cluster name"
+  //default     = ""
+}
+
+variable "cohort_demo_ecs_log_group" {
+  description = "ECS cluster cloud watch log group name"
   //default     = ""
 }
