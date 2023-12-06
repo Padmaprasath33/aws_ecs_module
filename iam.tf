c54c0920772531891d12dd763dde98b4aee1b88b
Padmaprasath <padmaprasath33@gmail.com>
2023-12-06 09:19:59 +0530

    Added ECS config

---
+++ iam.tf
@@ -0,0 +1,67 @@
+resource "aws_iam_role" "ecs_task_role" {
+  name = "cohort-demo-ecs-tas-role"
+ 
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "ecs_task_policy" {
+  name        = "cohort-demo-ecs-task-policy"
+  description = "Policy that allows access for ecs tasks to AWS services"
+ 
+ policy = <<EOF
+{
+   "Version": "2012-10-17",
+   "Statement": [
+       {
+           "Effect": "Allow",
+           "Action": "*",
+           "Resource": "*"
+       }
+   ]
+}
+EOF
+}
+ 
+resource "aws_iam_role_policy_attachment" "ecs-task-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.ecs_task_policy.arn
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "cohort-demo-ecs-task-execution-role"
+ 
+  assume_role_policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": "sts:AssumeRole",
+     "Principal": {
+       "Service": "ecs-tasks.amazonaws.com"
+     },
+     "Effect": "Allow",
+     "Sid": ""
+   }
+ ]
+}
+EOF
+}
+ 
+resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
