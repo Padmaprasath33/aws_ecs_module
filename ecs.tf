84d78873a17ee9d4f7b5128e79b01b2464774a9e
Padmaprasath <padmaprasath33@gmail.com>
2023-12-06 08:52:26 +0530

    Added ECS config

--- ecs.tf
+++ ecs.tf
@@ -28,4 +28,44 @@ resource "aws_cloudwatch_log_group" "cohort_demo_ecs_log_group" {
   name = var.cohort_demo_ecs_log_group
 }
 
+data "aws_ecr_image" "cohort_demo" {
+  repository_name = "cohort_demo"
+  most_recent       = true
+}
 
+resource "aws_ecs_task_definition" "cohort_demo_ui_task_definition" {
+  family = "cohort_demo_ui_task_definition"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.ecs_fargate_cpu
+  memory                   = var.ecs_fargate_memory
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  container_definitions = jsonencode([{
+   name        = "cohort_demo_ecs_container"
+   //412699049661.dkr.ecr.us-east-1.amazonaws.com/cohort_demo:5dab2de
+   image       = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.ecr_repo_name}:${data.aws_ecr_image.cohort_demo.image_tag}"
+   essential   = true
+   portMappings = [{
+     protocol      = "tcp"
+     containerPort = var.container_port
+     hostPort      = var.container_port
+   }]
+}])
+
+  volume {
+    name = var.efs_volume_name
+
+    efs_volume_configuration {
+      file_system_id          = var.aws_efs_file_system_id
+      root_directory          = "/opt/data"
+      transit_encryption      = "ENABLED"
+      transit_encryption_port = 2999
+      /*authorization_config {
+        access_point_id = var.aws_efs_access_point_id
+        iam             = "ENABLED"
+      }*/
+    }
+  }
+
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,48 @@ variable "cohort_demo_ecs_log_group" {
   description = "ECS cluster cloud watch log group name"
   //default     = ""
 }
+
+variable "ecs_fargate_cpu" {
+  description = "ECS fargate cpu"
+  //default     = ""
+}
+
+variable "ecs_fargate_memory" {
+  description = "ECS fargate memory"
+  //default     = ""
+}
+
+variable "aws_account_id" {
+  description = "AWS account ID"
+  //default     = ""
+}
+
+variable "region" {
+  description = "AWS region"
+  //default     = ""
+}
+
+variable "ecr_repo_name" {
+  description = "AWS ecr_repo_name"
+  //default     = ""
+}
+
+variable "container_port" {
+  description = "ECS container_port"
+  //default     = ""
+}
+
+variable "efs_volume_name" {
+  description = "AWS EFS volume name"
+  //default     = ""
+}
+
+variable "aws_efs_file_system_id" {
+  description = "AWS EFS file system id"
+  //default     = ""
+}
+
+variable "aws_efs_access_point_id" {
+  description = "AWS EFS access point id"
+  //default     = ""
+}
