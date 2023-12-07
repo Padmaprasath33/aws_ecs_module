0f749a781aa48a3c475462eea020ac94308b3416
Padmaprasath <padmaprasath33@gmail.com>
2023-12-07 09:14:06 +0530

    Added ECS config

--- alb.tf
+++ alb.tf
@@ -17,7 +17,8 @@ resource "aws_lb_target_group" "tg" {
   count = length(local.target_groups)
 
   name        = "cohort-demo-tg-${element(local.target_groups, count.index)}"
-  port        = 443
+  //port        = 443
+  port = 80
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = var.vpc_id
