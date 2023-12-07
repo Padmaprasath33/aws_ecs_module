f9bfde5a50624c9284cfe9497e85c66fb1ebf274
Padmaprasath <padmaprasath33@gmail.com>
2023-12-07 08:25:51 +0530

    Added ECS config

--- ecs.tf
+++ ecs.tf
@@ -53,7 +53,8 @@ resource "aws_ecs_task_definition" "cohort_demo_ui_task_definition" {
   container_definitions = jsonencode([{
    name        = "cohort_demo_ecs_container"
    //412699049661.dkr.ecr.us-east-1.amazonaws.com/cohort_demo:5dab2de
-   image       = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.ecr_repo_name}:${var.image_tag}"
+   //image       = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.ecr_repo_name}:${var.image_tag}"
+   image = "nginx"
    essential   = true
    "mountPoints": [
           {
@@ -103,7 +104,8 @@ resource "aws_ecs_task_definition" "cohort_demo_backend_task_definition" {
   container_definitions = jsonencode([{
    name        = "cohort_demo_ecs_container"
    //412699049661.dkr.ecr.us-east-1.amazonaws.com/cohort_demo:5dab2de
-   image       = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.ecr_repo_name}:${var.image_tag}"
+   //image       = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.ecr_repo_name}:${var.image_tag}"
+   image = "nginx"
    essential   = true
    "mountPoints": [
           {
