9d343c30868c5f2177743af3f0a6d0e0b188b88d
Padmaprasath <padmaprasath33@gmail.com>
2023-12-08 10:21:52 +0530

    Added ECS config

--- iam.tf
+++ iam.tf
@@ -95,7 +95,7 @@ EOF
 resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
   role       = aws_iam_role.ecs_task_execution_role.name
   //policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  policy_arn = aws.iam_policy.ecs_task_execution_policy
+  policy_arn = aws_iam_policy.ecs_task_execution_policy.arn
 }
 
 
