241f93c72d634954492ef1ee14210e0954810254
Padmaprasath <padmaprasath33@gmail.com>
2023-12-12 11:24:54 +0530

    Added datadog module

--- ecs.tf
+++ ecs.tf
@@ -60,12 +60,17 @@ resource "aws_ecs_task_definition" "cohort_demo_ui_task_definition" {
    //image       = "${var.aws_account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.ecr_repo_name}:${var.image_tag}"
    image = "nginx"
    essential   = true
-   /*"mountPoints": [
+   "mountPoints": [
           {
               "containerPath": "/usr/share/nginx/html",
               "sourceVolume": var.efs_volume_name
           }
-      ]*/
+      ]
+      "command": [
+        "cp",
+        "index.html",
+        "/usr/share/nginx/html"
+      ]
    portMappings = [{
      protocol      = "tcp"
      containerPort = var.container_port
@@ -81,7 +86,7 @@ resource "aws_ecs_task_definition" "cohort_demo_ui_task_definition" {
     }
 }])
 
-  /*volume {
+  volume {
     name = var.efs_volume_name
 
     efs_volume_configuration {
@@ -92,9 +97,9 @@ resource "aws_ecs_task_definition" "cohort_demo_ui_task_definition" {
       authorization_config {
         access_point_id = var.aws_efs_access_point_id
         iam             = "ENABLED"
-      }
+      }*/
     }
-  }*/
+  }
   tags = var.resource_tags
 }
 
